b3c5dc5fdd48d9ab43f5b52373b8a2f8dc3e327a
Hamza <jhamzaahmed@gmail.com>
2024-06-06 23:06:29 +0500

    completed sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -141,22 +141,55 @@ const Sidebar = ({
                 )}
               </FlexBetween>
             </Box>
-            <List>{navItems.map(({text, icon}) => {
-              if (!icon) {
-                return (
-                  <Typography key={text} sx={{m: '2.25rem 0 1rem 3rem'}}>
-                    {text}
-                  </Typography>
-                )
-              }
-              const lcText = text.toLowerCase()
+            <List>
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lcText = text.toLowerCase();
 
-              return (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton onClick={() => {navigate(`/${lcText}`); setActive(lcText)}}></ListItemButton>
-                </ListItem>
-              )
-            }}</List>
+                return (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      }}
+                      sx={{
+                        backgroundColor:
+                          active === lcText
+                            ? theme.palette.secondary[300]
+                            : "transparent",
+                        color:
+                          active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[100],
+                      }}
+                    >
+                      <ListItemIcon
+                        sx={{
+                          ml: "2rem",
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[200],
+                        }}
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lcText && (
+                        <ChevronRightOutlined sx={{ ml: "auto" }} />
+                      )}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
           </Box>
         </Drawer>
       )}
