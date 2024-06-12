3da70e2fd21b63f8ffbcac312c5251d750b3e9b4
Hamza <jhamzaahmed@gmail.com>
2024-06-12 22:13:04 +0500

    added dummy data in db

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 // CONFIGURATION
 dotenv.config();
 const app = express();
@@ -35,5 +39,7 @@ mongoose
     app.listen(PORT, () => {
       console.log(`Server started at http://localhost:${PORT}`);
     });
+    // only add data one time
+    User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
