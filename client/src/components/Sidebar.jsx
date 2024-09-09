ee6c91f5e86e8e4b60a8bad449c33ea532800c80
Hamza <jhamzaahmed@gmail.com>
2024-09-09 16:20:42 +0500

    completed fetching and display data

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import {
   Box,
   Divider,
@@ -93,6 +94,7 @@ const navItems = [
 ];
 
 const Sidebar = ({
+  user,
   drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
@@ -190,6 +192,38 @@ const Sidebar = ({
                 );
               })}
             </List>
+          </Box>
+          <Box position="absolute" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component={"img"}
+                alt="profile"
+                src={profileImg}
+                height={"40px"}
+                width={"40px"}
+                borderRadius={"50%"}
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign={"left"}>
+                <Typography
+                  fontWeight={"bold"}
+                  fontSize={"0.9rem"}
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize={"0.8rem"}
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
