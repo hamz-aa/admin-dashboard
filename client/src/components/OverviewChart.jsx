13de8099cb9a6a653db72809ab69179a7bb4602e
Hamza <jhamzaahmed@gmail.com>
2024-11-01 20:34:52 +0500

    added overview page

---
+++ client/src/components/OverviewChart.jsx
@@ -0,0 +1,13 @@
+import { ResponsiveLine } from "@nivo/line";
+import { useTheme } from "@mui/material";
+import { useGetSalesQuery } from "../state/api";
+
+const OverviewChart = ({ isDashboard = false, view }) => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetSalesQuery();
+  console.log(data, "data");
+
+  return <div>OverviewChart</div>;
+};
+
+export default OverviewChart;
